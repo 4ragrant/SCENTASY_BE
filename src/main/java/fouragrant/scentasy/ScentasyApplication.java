0c423b8e5768cb395c8a9fdb765cf727fb3977f4
Kayeon Seon <gyseon20@duksung.ac.kr>
2024-06-18 11:09:15 +0900

    프로젝트 생성

---
+++ src/main/java/fouragrant/scentasy/ScentasyApplication.java
@@ -0,0 +1,13 @@
+package fouragrant.scentasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScentasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScentasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fouragrant/scentasy/ScentasyApplicationTests.java
@@ -0,0 +1,13 @@
+package fouragrant.scentasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScentasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
