d7dfad8f79fc58ae3f93a7961be31761801f4220
seojjin <002seojin@gmail.com>
2024-08-24 16:39:39 +0900

    fix: api 명세 추가

--- src/main/java/fouragrant/scentasy/biz/member/cotroller/AuthController.java
+++ src/main/java/fouragrant/scentasy/biz/member/cotroller/AuthController.java
@@ -6,7 +6,13 @@ import fouragrant.scentasy.biz.member.dto.TokenDto;
 import fouragrant.scentasy.biz.member.dto.TokenReqDto;
 import fouragrant.scentasy.biz.member.service.AuthService;
 import fouragrant.scentasy.jwt.JwtBlacklist;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,22 +22,31 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthService authService;
 
+    @Operation(summary = "회원가입", description = "회원가입을 위한 메소드")
+    @ApiResponse(content = @Content(schema = @Schema(implementation = Response.class)))
     @PostMapping("/signup")
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<MemberResDto> signup(@RequestBody MemberReqDto memberReqDto) {
         return ResponseEntity.ok(authService.signup(memberReqDto));
     }
-
+    @Operation(summary = "로그인", description = "로그인을 위한 메소드")
+    @ApiResponse(content = @Content(schema = @Schema(implementation = Response.class)))
     @PostMapping("/login")
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<TokenDto> login(@RequestBody MemberReqDto memberDtoRes) {
         return ResponseEntity.ok(authService.login(memberDtoRes));
     }
-
+    @Operation(summary = "토큰재발급", description = "토큰재발급을 위한 메소드")
+    @ApiResponse(content = @Content(schema = @Schema(implementation = Response.class)))
     @PostMapping("/reissue")
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<TokenDto> reissue(@RequestBody TokenReqDto tokenRequestDto) {
         return ResponseEntity.ok(authService.reissue(tokenRequestDto));
     }
-
+    @Operation(summary = "로그아웃", description = "로그아웃을 위한 메소드")
+    @ApiResponse(content = @Content(schema = @Schema(implementation = Response.class)))
     @PostMapping("/logout")
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<String> logout(@RequestBody TokenReqDto tokenRequestDto) {
         return ResponseEntity.ok(authService.logout(tokenRequestDto));
     }
