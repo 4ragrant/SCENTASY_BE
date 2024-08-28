f091effb8ede639595b1281926dcc15429b2fb9b
kayeon2 <tjsrkdus@gmail.com>
2024-08-28 23:44:45 +0900

    fix: 닉네임 중복 예외처리 추가

--- src/main/java/fouragrant/scentasy/biz/member/service/ExtraInfoService.java
+++ src/main/java/fouragrant/scentasy/biz/member/service/ExtraInfoService.java
@@ -4,6 +4,8 @@ import fouragrant.scentasy.biz.member.domain.ExtraInfo;
 import fouragrant.scentasy.biz.member.domain.Member;
 import fouragrant.scentasy.biz.member.dto.ExtraInfoReqDto;
 import fouragrant.scentasy.biz.member.repository.ExtraInfoRepository;
+import fouragrant.scentasy.common.exception.CommonException;
+import fouragrant.scentasy.common.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,10 @@ public class ExtraInfoService {
     private final ExtraInfoRepository repository;
 
     public ExtraInfo saveExtraInfo(ExtraInfoReqDto extraInfoReqDto, Member member) {
+        if (repository.existsByNickname(extraInfoReqDto.getNickname())) {
+            throw new CommonException(ErrorCode.NICKNAME_DUPLICATED);
+        }
+
         ExtraInfo extraInfo = ExtraInfo.fromDto(extraInfoReqDto, member);
         return repository.save(extraInfo);
     }
