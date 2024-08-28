952c8fe2a6734f3a3463ab43cd99acd87e4d7692
nar0ng <71819594+nar0ng@users.noreply.github.com>
2024-08-28 18:58:48 +0900

    refact: 에러형식 중 에러코드의 형식을 Integer에서 String 자료형으로 변경

--- src/main/java/fouragrant/scentasy/common/Response.java
+++ src/main/java/fouragrant/scentasy/common/Response.java
@@ -3,6 +3,7 @@ package fouragrant.scentasy.common;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @NoArgsConstructor
@@ -12,22 +13,22 @@ public class Response<T> {
     private static final String ERROR_RESULT = "error";
 
     private String message;
-    private Integer code;
+    private String code;
     private T data;
 
     public static <T> Response<T> createSuccessWithNoMessage(T data) {
         return new Response<>(SUCCESS_RESULT, null, data);
     }
 
-    public static <T> Response<T> createSuccess(Integer code, T data) {
+    public static <T> Response<T> createSuccess(String code, T data) {
         return new Response<>(SUCCESS_RESULT, code, data);
     }
 
-    public static Response<?> createSuccessWithNoData(Integer code) {
+    public static Response<?> createSuccessWithNoData(String code) {
         return new Response<>(SUCCESS_RESULT, code, null);
     }
 
-    public static Response<?> createError(Integer code, String string) {
+    public static Response<?> createError(String code, String message) {
         return new Response<>(ERROR_RESULT, code, null);
     }
 }
