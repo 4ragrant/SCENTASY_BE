3cca0183e2d71e609b117c6ff3382b3d2c8ff23c
nar0ng <71819594+nar0ng@users.noreply.github.com>
2024-08-28 18:33:40 +0900

    refact: errorCode code string에서 integer로 형식 변경

--- src/main/java/fouragrant/scentasy/common/Response.java
+++ src/main/java/fouragrant/scentasy/common/Response.java
@@ -12,22 +12,22 @@ public class Response<T> {
     private static final String ERROR_RESULT = "error";
 
     private String message;
-    private String code;
+    private Integer code;
     private T data;
 
     public static <T> Response<T> createSuccessWithNoMessage(T data) {
         return new Response<>(SUCCESS_RESULT, null, data);
     }
 
-    public static <T> Response<T> createSuccess(String code, T data) {
+    public static <T> Response<T> createSuccess(Integer code, T data) {
         return new Response<>(SUCCESS_RESULT, code, data);
     }
 
-    public static Response<?> createSuccessWithNoData(String code) {
+    public static Response<?> createSuccessWithNoData(Integer code) {
         return new Response<>(SUCCESS_RESULT, code, null);
     }
 
-    public static Response<?> createError(String code) {
+    public static Response<?> createError(Integer code, String string) {
         return new Response<>(ERROR_RESULT, code, null);
     }
 }
