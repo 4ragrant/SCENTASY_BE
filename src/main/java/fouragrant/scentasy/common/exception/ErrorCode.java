17ad1d4268053cc742047e25235006e03a376a33
nar0ng <71819594+nar0ng@users.noreply.github.com>
2024-08-28 18:13:01 +0900

    refact: errorCode에 블랙리스트 예외 추가

--- src/main/java/fouragrant/scentasy/common/exception/ErrorCode.java
+++ src/main/java/fouragrant/scentasy/common/exception/ErrorCode.java
@@ -10,8 +10,8 @@ public enum ErrorCode {
 
     EMAIL_DUPLICATED(4201, HttpStatus.CONFLICT, "존재하는 이메일입니다."),
     FAILURE_LOGIN(4300, HttpStatus.UNAUTHORIZED, "잘못된 아이디 또는 비밀번호입니다."),
-    EXPIRED_TOKEN_ERROR(4400, HttpStatus.UNAUTHORIZED, "만료된 토큰입니다.");
-
+    EXPIRED_TOKEN_ERROR(4400, HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    TOKEN_IN_BLACKLIST(4401,HttpStatus.UNAUTHORIZED, "블랙리스트 토큰." );
     private final Integer code;
     private final HttpStatus httpStatus;
     private final String message;
