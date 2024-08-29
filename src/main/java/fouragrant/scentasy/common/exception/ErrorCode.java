c449db23e56d14c33dcfee75dc69bbbaf1846ba9
kayeon2 <tjsrkdus@gmail.com>
2024-08-29 17:39:24 +0900

    refac: 추가정보 누락 에러코드 추가

--- src/main/java/fouragrant/scentasy/common/exception/ErrorCode.java
+++ src/main/java/fouragrant/scentasy/common/exception/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
     EMAIL_DUPLICATED("4201", HttpStatus.CONFLICT, "존재하는 이메일입니다."),
     NICKNAME_DUPLICATED("4202", HttpStatus.CONFLICT, "존재하는 닉네임입니다."),
     FAILURE_LOGIN("4300", HttpStatus.UNAUTHORIZED, "잘못된 아이디 또는 비밀번호입니다."),
+    EXTRA_INFO_NOT_FOUND("4301", HttpStatus.UNAUTHORIZED, "추가정보가 존재하지 않습니다."),
     TOKEN_EXPIRED("4400", HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
     TOKEN_IN_BLACKLIST("4401", HttpStatus.UNAUTHORIZED, "블랙리스트 토큰." );
 
