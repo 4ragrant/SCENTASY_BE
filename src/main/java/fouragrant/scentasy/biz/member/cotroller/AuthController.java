264189dfe7b8476b380fb81425438fbd62249797
nar0ng <71819594+nar0ng@users.noreply.github.com>
2024-08-31 09:50:50 +0900

    style: auth 관련 스웨거 수정

--- src/main/java/fouragrant/scentasy/biz/member/cotroller/AuthController.java
+++ src/main/java/fouragrant/scentasy/biz/member/cotroller/AuthController.java
@@ -8,6 +8,7 @@ import fouragrant.scentasy.biz.member.service.AuthService;
 import fouragrant.scentasy.common.Response;
 import fouragrant.scentasy.common.exception.ErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -32,7 +33,20 @@ public class AuthController {
     private final AuthService authService;
 
     @Operation(summary = "회원가입", description = "회원가입을 위한 메소드")
-    @ApiResponse(content = @Content(schema = @Schema(implementation = MemberReqDto.class)))
+    @ApiResponse(responseCode = "0000", description = "signup successfully",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = MemberResDto.class)
+            )
+    )
+    @ApiResponse(responseCode = "4201", description = "존재하는 이메일입니다.")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "회원가입 요청 본문",
+            required = true,
+            content = @Content(
+                    schema = @Schema(implementation = MemberReqDto.class)
+            )
+    )
     @PostMapping("/signup")
     public  ResponseEntity<?> signup(@RequestBody @Valid MemberReqDto memberReqDto, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
@@ -42,8 +56,23 @@ public class AuthController {
         return ResponseEntity.ok(Response.createSuccess("0000", memberResDto));
     }
 
-    @Operation(summary = "일반 로그인", description = "일반 로그인을 위한 메소드")
-    @ApiResponse(content = @Content(schema = @Schema(implementation = MemberReqDto.class)))
+    @Operation(summary = "로그인", description = "로그인 위한 메소드")
+    @ApiResponse(responseCode = "0000", description = "login successfully",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = TokenDto.class)
+            )
+    )
+    @ApiResponse(responseCode = "4300", description = "잘못된 이메일 또는 비밀번호입니다")
+    @ApiResponse(responseCode = "4301", description = "추가정보가 존재하지 않습니다")
+    @ApiResponse(responseCode = "4400", description = "만료된 토큰입니다")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "로그인 요청 본문",
+            required = true,
+            content = @Content(
+                    schema = @Schema(implementation = MemberReqDto.class)
+            )
+    )
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody MemberReqDto memberDtoRes) {
         TokenDto tokenDto = authService.login(memberDtoRes);
@@ -51,7 +80,19 @@ public class AuthController {
     }
 
     @Operation(summary = "리이슈", description = "리이슈 및 토큰 재발급을 위한 메소드")
-    @ApiResponse(content = @Content(schema = @Schema(implementation = TokenDto.class)))
+    @ApiResponse(responseCode = "0000", description = "login successfully",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = TokenDto.class)
+            )
+    )
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "리이슈 요청 본문",
+            required = true,
+            content = @Content(
+                    schema = @Schema(implementation = TokenReqDto.class)
+            )
+    )
     @PostMapping("/reissue")
     public ResponseEntity<?> reissue(@RequestBody TokenReqDto tokenRequestDto) {
         TokenDto tokenDto = authService.reissue(tokenRequestDto);
@@ -59,7 +100,14 @@ public class AuthController {
     }
 
     @Operation(summary = "로그아웃", description = "로그아웃을 위한 메소드")
-    @ApiResponse(content = @Content(schema = @Schema(implementation = TokenDto.class)))
+    @ApiResponse(responseCode = "0000", description = "logout successfully")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "로그아웃 요청 본문",
+            required = true,
+            content = @Content(
+                    schema = @Schema(implementation = TokenReqDto.class)
+            )
+    )
     @PostMapping("/logout")
     public ResponseEntity<?> logout(@RequestBody TokenReqDto tokenRequestDto) {
         authService.logout(tokenRequestDto);
