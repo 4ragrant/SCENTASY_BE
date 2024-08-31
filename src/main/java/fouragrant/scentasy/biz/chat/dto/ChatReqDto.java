caca791f32d6e6c2bcfaa639d42d9f36bd66fbe7
nar0ng <71819594+nar0ng@users.noreply.github.com>
2024-08-31 10:50:24 +0900

    refact: 채팅할 때 사용자의 추가정보도 함께 전송하도록 수정

--- src/main/java/fouragrant/scentasy/biz/chat/dto/ChatReqDto.java
+++ src/main/java/fouragrant/scentasy/biz/chat/dto/ChatReqDto.java
@@ -1,14 +1,13 @@
 package fouragrant.scentasy.biz.chat.dto;
 
-import fouragrant.scentasy.biz.member.domain.ExtraInfo;
+import fouragrant.scentasy.biz.member.dto.ExtraInfoReqDto;
 import lombok.Builder;
-import lombok.Data;
 
 
 @Builder
 public record ChatReqDto(
         String input,
-        ExtraInfo extraInfo
+        ExtraInfoReqDto extraInfo
 ) {
 
 }
