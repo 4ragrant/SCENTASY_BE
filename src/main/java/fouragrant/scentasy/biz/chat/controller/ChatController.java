9c76a046a4380cb1c2aec020ac1f8a601a408840
nar0ng <71819594+nar0ng@users.noreply.github.com>
2024-08-31 09:26:45 +0900

    style: chat 관련 스웨거 작성

--- src/main/java/fouragrant/scentasy/biz/chat/controller/ChatController.java
+++ src/main/java/fouragrant/scentasy/biz/chat/controller/ChatController.java
@@ -6,6 +6,14 @@ import fouragrant.scentasy.biz.chat.dto.ChatReqDto;
 import fouragrant.scentasy.biz.chat.dto.ChatResDto;
 import fouragrant.scentasy.biz.chat.service.ChatService;
 import fouragrant.scentasy.common.Response;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
@@ -20,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@Tag(name = "Chat", description = "Chat 관련 api")
 @RestController
 @RequestMapping("/api/chats")
 @RequiredArgsConstructor
@@ -27,6 +36,16 @@ public class ChatController {
 
     private final ChatService chatService;
 
+    @Operation(summary = "채팅", description = "채팅을 위한 메소드")
+    @ApiResponse(responseCode = "0000", description = "chat successfully!",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ChatResDto.class)
+            )
+    )
+    @Parameters({
+            @Parameter(name = "memberId", description = "멤버의 ID, path variable")
+    })
     @PostMapping("/{memberId}")
     public ResponseEntity<?> chat(@RequestBody ChatReqDto chatReqDto,
                                   @PathVariable Long memberId) {
@@ -34,12 +53,32 @@ public class ChatController {
             return ResponseEntity.ok(Response.createSuccess("0000", chatResDto));
     }
 
+    @Operation(summary = "멤버별 채팅 날짜 리스트 조회", description = "멤버별 채팅 내역이 있는 날짜 조회를 위한 메소드")
+    @ApiResponse(responseCode = "0000", description = "Successful retrieve chat dates",
+            content = @Content(
+                    mediaType = "application/json"
+            )
+    )
+    @Parameters({
+            @Parameter(name = "memberId", description = "멤버의 ID, path variable")
+    })
     @GetMapping("/dates/{memberId}")
     public ResponseEntity<Response<List<Date>>> getChatDatesByMemberId(@PathVariable Long memberId) {
         List<Date> chatDates = chatService.getChatDatesByMemberId(memberId);
         return ResponseEntity.ok(Response.createSuccess("0000", chatDates));
     }
 
+    @Operation(summary = "멤버별 날짜별 채팅 내역 조회", description = "멤버별 날짜별 채팅 내역 조회를 위한 메소드")
+    @ApiResponse(responseCode = "0000", description = "Successful retrieve chat lists",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ChatListResDto.class)
+            )
+    )
+    @Parameters({
+            @Parameter(name = "memberId", description = "멤버의 ID, path variable"),
+            @Parameter(name = "date", description = "날,, path variable")
+    })
     @GetMapping("/{memberId}/{date}")
     public ResponseEntity<Response<List<ChatListResDto>>> getChatsByMemberIdAndDate(@PathVariable Long memberId,
                                                                                     @PathVariable String date) {
