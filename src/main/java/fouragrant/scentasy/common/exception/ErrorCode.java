f7c0680b077bd425be9ee120c2ebc16b97d8ffda
kayeon2 <tjsrkdus@gmail.com>
2024-09-04 12:36:01 +0900

    refac: 향수 정보 없음 에러코드 추가

--- src/main/java/fouragrant/scentasy/common/exception/ErrorCode.java
+++ src/main/java/fouragrant/scentasy/common/exception/ErrorCode.java
@@ -14,7 +14,8 @@ public enum ErrorCode {
     FAILURE_LOGIN("4300", HttpStatus.UNAUTHORIZED, "잘못된 아이디 또는 비밀번호입니다."),
     EXTRA_INFO_NOT_FOUND("4301", HttpStatus.UNAUTHORIZED, "추가정보가 존재하지 않습니다."),
     TOKEN_EXPIRED("4400", HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
-    TOKEN_IN_BLACKLIST("4401", HttpStatus.UNAUTHORIZED, "블랙리스트 토큰." );
+    TOKEN_IN_BLACKLIST("4401", HttpStatus.UNAUTHORIZED, "블랙리스트 토큰." ),
+    PERFUME_NOT_FOUND("4500", HttpStatus.BAD_REQUEST, "해당 향수가 존재하지 않습니다.");
 
     private final String code;
     private final HttpStatus httpStatus;
