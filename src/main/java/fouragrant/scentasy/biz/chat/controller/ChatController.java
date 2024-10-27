bfe4cc9262e214babdccf7856e807ccd99537eb9
nar0ng <71819594+nar0ng@users.noreply.github.com>
2024-10-27 20:35:35 +0900

    refact: 사용하지 않는 import문 삭제

--- src/main/java/fouragrant/scentasy/biz/chat/controller/ChatController.java
+++ src/main/java/fouragrant/scentasy/biz/chat/controller/ChatController.java
@@ -14,8 +14,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.sql.Date;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
--- src/main/java/fouragrant/scentasy/biz/chat/repository/ChatRepository.java
+++ src/main/java/fouragrant/scentasy/biz/chat/repository/ChatRepository.java
@@ -1,12 +1,9 @@
 package fouragrant.scentasy.biz.chat.repository;
 
 import fouragrant.scentasy.biz.chat.domain.Chat;
-import java.time.LocalDateTime;
-import java.sql.Date;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
